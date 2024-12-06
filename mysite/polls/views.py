f5d14f6d7d91ba6310de59eb4b81298a19068184
VitttC <vittt.caria@gmail.com>
2024-12-06 05:02:46 +0100

    Part III: added views and templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Welcome, you are at the polls' index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
+    
+    #ONLY FOR PART I: return HttpResponse("Welcome, you are at the polls' index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." %question_id)
+
+
+def results(request, question_id):
+    response = "You are looking at results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." %question_id)
