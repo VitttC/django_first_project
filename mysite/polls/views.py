e25cd4f43c956271838b017d4b4a062963fd85e6
VitttC <vittt.caria@gmail.com>
2024-12-06 17:49:43 +0100

    Part III.1: modified views/urls referencing

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 
 from .models import Question
 
@@ -11,12 +12,11 @@ def index(request):
         "latest_question_list": latest_question_list
     }
     return render(request, "polls/index.html", context)
-    
-    #ONLY FOR PART I: return HttpResponse("Welcome, you are at the polls' index.")
 
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
